e751da8a184d843bd0a83f4a105a3e1bc8b2f0bd
Oleg Golevitch <oleg133766@gmail.com>
2022-10-06 17:20:49 +0300

    :wrench: Prettier and Esling Configuration Update
    Added Prettier Configuration file and updated Eslint configuration file + Deleted useless things

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link>
     </nav>
     <router-view/>
   </div>
@@ -15,6 +14,9 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+html{
+  background-color: rgba(22, 22, 26, 1);
 }
 
 nav {
